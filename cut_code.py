090c4db9dac24c784c6f76ebc3a906638d2606ae
v4lue4dded <v4lue4dded@gmail.com>
2020-04-20 11:29:55 +0100

    added data about tests

---
+++ cut_code.py
@@ -0,0 +1,71 @@
+df_co  = pd.read_csv(data_path+ 'csse_covid_19_time_series//time_series_covid19_confirmed_global.csv')
+df_re  = pd.read_csv(data_path+ 'csse_covid_19_time_series//time_series_covid19_recovered_global.csv')
+df_de  = pd.read_csv(data_path+ 'csse_covid_19_time_series//time_series_covid19_deaths_global.csv')
+
+
+
+df_lu.columns
+df_co.columns
+df_re.columns
+df_de.columns
+
+id_vars = ['Province/State', 'Country/Region', 'Lat', 'Long']
+
+def clean_df(df, id_vars):
+    value_vars = set(df.columns) - set(id_vars)
+    return pd.melt(
+        df
+        , id_vars=id_vars
+        , value_vars=value_vars
+        , var_name='date'
+        , value_name='number'
+    ).assign(
+        date              = lambda x: pd.to_datetime(x.date,format = '%m/%d/%y')
+        , province_state  = lambda x: x['Province/State'].fillna('')
+        , country_region  = lambda x: x['Country/Region']
+    )[['country_region', 'province_state', 'date', 'number']]
+
+df_co_clean = clean_df(df_co, id_vars).assign(confirmed = lambda x: x.number).drop(columns = 'number')
+df_re_clean = clean_df(df_re, id_vars).assign(recovered = lambda x: x.number).drop(columns = 'number')
+df_de_clean = clean_df(df_de, id_vars).assign(deaths    = lambda x: x.number).drop(columns = 'number')
+
+
+
+df_data_clean = df_lu_clean.merge(
+    df_co_clean, how = 'outer', on = ['country_region', 'province_state']).merge(
+    df_re_clean, how = 'outer', on = ['country_region', 'province_state', 'date']).merge(
+    df_de_clean, how = 'outer', on = ['country_region', 'province_state', 'date']).assign(
+          confirmed = lambda x: x['confirmed'].fillna(0)
+        , recovered = lambda x: x['recovered'].fillna(0)
+        , deaths    = lambda x: x['deaths'   ].fillna(0)
+        , active    = lambda x: (x.confirmed - x.deaths - x.recovered).fillna(0)
+    )[
+        ["lu_id", 'country_region', 'province_state', 'date', 'confirmed', 'active', 'deaths', 'recovered']
+    ].query("date.notnull()", engine = "python").assign(
+        lu_id = lambda x: x.lu_id.fillna(-1)
+    ,   lag_1_confirmed = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['confirmed'].shift(1).fillna(0)
+    ,   lag_1_recovered = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['recovered'].shift(1).fillna(0)
+    ,   lag_1_deaths    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['deaths'   ].shift(1).fillna(0)
+    ,   lag_1_active    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['active'   ].shift(1).fillna(0)
+    ,   lag_7_confirmed = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['confirmed'].shift(7).fillna(0)
+    ,   lag_7_recovered = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['recovered'].shift(7).fillna(0)
+    ,   lag_7_deaths    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['deaths'   ].shift(7).fillna(0)
+    ,   lag_7_active    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['active'   ].shift(7).fillna(0)
+    ).query('''confirmed > 0 or recovered > 0 or deaths > 0 or active> 0'''
+    )
+
+
+
+
+
+
+
+
+    
+max(df_lu_clean[["iso3", "country_region", "province_state"]].drop_duplicates().iso3.value_counts())
+max(df_lu_clean[["iso3", "country_region", "province_state"]].drop_duplicates().country_region.value_counts()) 
+
+df_lu_clean.shape
+df_co_clean.groupby(["country_region", "province_state"]).ngroups
+df_co_clean.groupby(["date"]).ngroups
+df_lu_clean.groupby(["iso3"]).ngroups
--- merge_tables.py
+++ merge_tables.py
@@ -7,39 +7,30 @@ import datetime as dt
 data_path = os.getcwd() + '\\COVID-19\\csse_covid_19_data\\'
 print(data_path)
 
-df_lu  = pd.read_csv(data_path+ 'UID_ISO_FIPS_LookUp_Table.csv')
-df_co  = pd.read_csv(data_path+ 'csse_covid_19_time_series//time_series_covid19_confirmed_global.csv')
-df_re  = pd.read_csv(data_path+ 'csse_covid_19_time_series//time_series_covid19_recovered_global.csv')
-df_de  = pd.read_csv(data_path+ 'csse_covid_19_time_series//time_series_covid19_deaths_global.csv')
+df_lu = pd.read_csv(data_path+ 'UID_ISO_FIPS_LookUp_Table.csv')
+df_co = pd.read_csv(data_path+ 'csse_covid_19_time_series//time_series_covid19_confirmed_global.csv')
+df_re = pd.read_csv(data_path+ 'csse_covid_19_time_series//time_series_covid19_recovered_global.csv')
+df_de = pd.read_csv(data_path+ 'csse_covid_19_time_series//time_series_covid19_deaths_global.csv')
 
 df_europe  = pd.read_csv(os.getcwd() + '//european_countries.tsv', sep='\t')
 
-
+df_owid = pd.read_csv(os.getcwd() + '\\covid-19-data\\public\\data\\owid-covid-data.csv')
 
 df_lu.columns
 df_co.columns
 df_re.columns
 df_de.columns
 
+country_regions_to_group = set([
+     "Canada"         #because of Problems within John Hopkins Dataset
+    ,"China"          #because of Problems merging with owid
+    ,"Australia"      #because of Problems merging with owid   
+    ,"United Kingdom" #because of Problems merging with owid   
+    ])
 
-df_country_map_names = pd.DataFrame(
-{ 'country_region'    : ['US'                      ,'Congo (Brazzaville)', 'Congo (Kinshasa)','Burma'  ,"Cote d'Ivoire", 'South Sudan', 'Central African Republic','Korea, South']
-, 'country_region_map': ['United States of America','Congo'              , 'Dem. Rep. Congo' ,'Myanmar',"Côte d'Ivoire", 'S. Sudan'   , 'Central African Rep.'    ,'South Korea' ]  
-})
+join_columns = ['country_region', 'province_state', 'date']
 
-df_lu_clean = df_lu.assign(
-      province_state  = lambda x: x['Province_State'].fillna('')
-    , country_region  = lambda x: x['Country_Region']
-    , lu_id           = lambda x: range(x.shape[0])
-).drop(columns = ['Province_State', 'Country_Region']).merge(
-    df_country_map_names, how = 'outer', on = ['country_region']
-).merge(
-    df_europe, how = 'outer', on = ['iso2']
-).assign(
-    country_region_map = lambda x: x.country_region_map.fillna(x.country_region)
-    , country_group = lambda x: x.country_group.fillna('')
-)
-
+df_co['Country/Region'].isin(country_regions_to_group)
 id_vars = ['Province/State', 'Country/Region', 'Lat', 'Long']
 
 def clean_df(df, id_vars):
@@ -52,37 +43,98 @@ def clean_df(df, id_vars):
         , value_name='number'
     ).assign(
         date              = lambda x: pd.to_datetime(x.date,format = '%m/%d/%y')
-        , province_state  = lambda x: x['Province/State'].fillna('')
+        , province_state  = lambda x: np.where(x['Country/Region'].isin(country_regions_to_group), '', x['Province/State'].fillna(''))
         , country_region  = lambda x: x['Country/Region']
-    )[['country_region', 'province_state', 'date', 'number']]
+    )[join_columns + ['number']].groupby(join_columns).sum().reset_index()
 
 df_co_clean = clean_df(df_co, id_vars).assign(confirmed = lambda x: x.number).drop(columns = 'number')
 df_re_clean = clean_df(df_re, id_vars).assign(recovered = lambda x: x.number).drop(columns = 'number')
 df_de_clean = clean_df(df_de, id_vars).assign(deaths    = lambda x: x.number).drop(columns = 'number')
 
+assert df_co_clean[join_columns].equals(df_re_clean[join_columns]),  "df_co_clean[join_columns] != df_re_clean[join_columns]"
+assert df_re_clean[join_columns].equals(df_de_clean[join_columns]),  "df_re_clean[join_columns] != df_de_clean[join_columns]"
 
 
-df_data_clean = df_lu_clean.merge(
-    df_co_clean, how = 'outer', on = ['country_region', 'province_state']).merge(
-    df_re_clean, how = 'outer', on = ['country_region', 'province_state', 'date']).merge(
-    df_de_clean, how = 'outer', on = ['country_region', 'province_state', 'date']).assign(
+df_country_map_names = pd.DataFrame(
+{ 'country_region'    : ['US'                      ,'Congo (Brazzaville)', 'Congo (Kinshasa)','Burma'  ,"Cote d'Ivoire", 'South Sudan', 'Central African Republic','Korea, South']
+, 'country_region_map': ['United States of America','Congo'              , 'Dem. Rep. Congo' ,'Myanmar',"Côte d'Ivoire", 'S. Sudan'   , 'Central African Rep.'    ,'South Korea' ]  
+})
+
+df_lu_rename = df_lu.assign(
+      province_state  = lambda x: x['Province_State'].fillna('')
+    , country_region  = lambda x: x['Country_Region']
+    , lu_id           = lambda x: range(x.shape[0])
+).drop(columns = ['Province_State', 'Country_Region']).merge(
+    df_country_map_names, how = 'outer', on = ['country_region']
+).merge(
+    df_europe, how = 'outer', on = ['iso2']
+).assign(
+      country_region_map = lambda x: x.country_region_map.fillna(x.country_region)
+    , country_group = lambda x: x.country_group.fillna('')
+)
+
+unique_crps = df_co_clean[['country_region', 'province_state']].drop_duplicates().reset_index()
+
+# check that all country_regions are in lookup table
+assert df_lu_rename.merge(unique_crps, how = "inner").shape[0] == unique_crps.shape[0]
+df_lu_clean = df_lu_rename.merge(unique_crps, how = "inner")
+
+# only two rows that don't have iso3
+assert df_lu_clean[df_lu_clean.iso3.isna()].shape[0] == 2
+
+df_te = df_owid.merge(
+      df_lu_clean[df_lu_clean.iso3.notna()]
+    , how = 'outer'
+    , left_on='iso_code'
+    , right_on='iso3'
+    , validate="m:1"    
+).assign(
+   ones = 1
+,  iso_code        = lambda x: x.iso_code.fillna("missing")
+,  iso3            = lambda x: x.iso3.fillna("missing")
+,  location        = lambda x: x.location.fillna("missing")
+,  country_region  = lambda x: x.country_region.fillna("missing")
+)
+
+# #TODO: Fix this to make it empty
+# assert df_te.groupby([
+#   "iso_code"      
+# , "iso3"          
+# , "location"      
+# , "country_region"]).sum().reset_index().query(
+# "iso_code == 'missing' or iso3 == 'missing' or location == 'missing' or country_region == 'missing'"
+# ).shape[0] == 0
+
+df_te_clean = df_te.assign(
+      date   = lambda x: pd.to_datetime(x.date,format = '%Y-%m-%d')
+    , tested = lambda x: x.total_tests
+)[join_columns + ['tested']].groupby(join_columns).sum().reset_index()
+
+df_data_clean = df_co_clean.merge(
+    df_re_clean, how = 'inner', validate = "1:1", on = join_columns).merge(
+    df_de_clean, how = 'inner', validate = "1:1", on = join_columns).merge(
+    df_te_clean, how = 'inner', validate = "1:1", on = join_columns).merge(
+    df_lu_clean, how = 'inner', validate = "m:1", on = ['country_region', 'province_state']).assign(
           confirmed = lambda x: x['confirmed'].fillna(0)
         , recovered = lambda x: x['recovered'].fillna(0)
         , deaths    = lambda x: x['deaths'   ].fillna(0)
+        , tested    = lambda x: x['tested'   ].fillna(0)
         , active    = lambda x: (x.confirmed - x.deaths - x.recovered).fillna(0)
     )[
-        ["lu_id", 'country_region', 'province_state', 'date', 'confirmed', 'active', 'deaths', 'recovered']
+        ["lu_id", 'country_region', 'province_state', 'date', 'confirmed', 'recovered', 'deaths', 'tested', 'active']
     ].query("date.notnull()", engine = "python").assign(
         lu_id = lambda x: x.lu_id.fillna(-1)
     ,   lag_1_confirmed = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['confirmed'].shift(1).fillna(0)
     ,   lag_1_recovered = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['recovered'].shift(1).fillna(0)
     ,   lag_1_deaths    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['deaths'   ].shift(1).fillna(0)
+    ,   lag_1_tested    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['tested'   ].shift(1).fillna(0)
     ,   lag_1_active    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['active'   ].shift(1).fillna(0)
     ,   lag_7_confirmed = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['confirmed'].shift(7).fillna(0)
     ,   lag_7_recovered = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['recovered'].shift(7).fillna(0)
     ,   lag_7_deaths    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['deaths'   ].shift(7).fillna(0)
+    ,   lag_7_tested    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['tested'   ].shift(7).fillna(0)
     ,   lag_7_active    = lambda x: x.sort_values(by=['date'], ascending=True).groupby(['lu_id'])['active'   ].shift(7).fillna(0)
-    ).query('''confirmed > 0 or recovered > 0 or deaths > 0 or active> 0'''
+    ).query('''confirmed > 0 or recovered > 0 or deaths > 0 or tested > 0 or active> 0'''
     )
 
 df_lu_clean.to_csv("df_lu_clean.tsv", index = False, sep = '\t', encoding='utf-8-sig')
@@ -129,16 +181,27 @@ print(power_bi_type_cast(df_lu_clean))
 print(power_bi_type_cast(df_data_clean))
 
 
-daily growth confirmed prev day    = DIVIDE(SUM('data_at'[confirmed]), SUM('data_at'[lag_1_confirmed]))       -1
-daily growth recovered prev day    = DIVIDE(SUM('data_at'[recovered]), SUM('data_at'[lag_1_recovered]))       -1
-daily growth deaths prev day       = DIVIDE(SUM('data_at'[deaths]   ), SUM('data_at'[lag_1_deaths]   ))       -1
-daily growth active prev day       = DIVIDE(SUM('data_at'[active]   ), SUM('data_at'[lag_1_active]   ))       -1
+# daily growth active prev day            = COALESCE(DIVIDE(SUM('data_at'[active]   ), SUM('data_at'[lag_1_active]   )),1)       - 1
+# daily growth confirmed prev day         = COALESCE(DIVIDE(SUM('data_at'[confirmed]), SUM('data_at'[lag_1_confirmed])),1)       - 1
+# daily growth deaths prev day            = COALESCE(DIVIDE(SUM('data_at'[deaths]   ), SUM('data_at'[lag_1_deaths]   )),1)       - 1
+# daily growth recovered prev day         = COALESCE(DIVIDE(SUM('data_at'[recovered]), SUM('data_at'[lag_1_recovered])),1)       - 1
+# daily growth active prev week           = COALESCE(DIVIDE(SUM('data_at'[active]   ), SUM('data_at'[lag_7_active]   )),1)^(1/7) - 1      
+# daily growth confirmed prev week        = COALESCE(DIVIDE(SUM('data_at'[confirmed]), SUM('data_at'[lag_7_confirmed])),1)^(1/7) - 1      
+# daily growth deaths prev week           = COALESCE(DIVIDE(SUM('data_at'[deaths]   ), SUM('data_at'[lag_7_deaths]   )),1)^(1/7) - 1      
+# daily growth recovered prev week        = COALESCE(DIVIDE(SUM('data_at'[recovered]), SUM('data_at'[lag_7_recovered])),1)^(1/7) - 1      
+# hist daily growth active prev day       = COALESCE(DIVIDE(SUM('data_ot'[active]   ), SUM('data_ot'[lag_1_active]   )),1)       - 1
+# hist daily growth confirmed prev day    = COALESCE(DIVIDE(SUM('data_ot'[confirmed]), SUM('data_ot'[lag_1_confirmed])),1)       - 1
+# hist daily growth deaths prev day       = COALESCE(DIVIDE(SUM('data_ot'[deaths]   ), SUM('data_ot'[lag_1_deaths]   )),1)       - 1
+# hist daily growth recovered prev day    = COALESCE(DIVIDE(SUM('data_ot'[recovered]), SUM('data_ot'[lag_1_recovered])),1)       - 1
+# hist daily growth active prev week      = COALESCE(DIVIDE(SUM('data_ot'[active]   ), SUM('data_ot'[lag_7_active]   )),1)^(1/7) - 1
+# hist daily growth confirmed prev week   = COALESCE(DIVIDE(SUM('data_ot'[confirmed]), SUM('data_ot'[lag_7_confirmed])),1)^(1/7) - 1
+# hist daily growth deaths prev week      = COALESCE(DIVIDE(SUM('data_ot'[deaths]   ), SUM('data_ot'[lag_7_deaths]   )),1)^(1/7) - 1
+# hist daily growth recovered prev week   = COALESCE(DIVIDE(SUM('data_ot'[recovered]), SUM('data_ot'[lag_7_recovered])),1)^(1/7) - 1
 
-hist daily growth confirmed prev day    = DIVIDE(SUM('data_ot'[confirmed]), SUM('data_ot'[lag_1_confirmed]))       -1
-hist daily growth recovered prev day    = DIVIDE(SUM('data_ot'[recovered]), SUM('data_ot'[lag_1_recovered]))       -1
-hist daily growth deaths prev day       = DIVIDE(SUM('data_ot'[deaths]   ), SUM('data_ot'[lag_1_deaths]   ))       -1
-hist daily growth active prev day       = DIVIDE(SUM('data_ot'[active]   ), SUM('data_ot'[lag_1_active]   ))       -1
-hist daily growth confirmed prev week   = DIVIDE(SUM('data_ot'[confirmed]), SUM('data_ot'[lag_7_confirmed]))^(1/7) -1
-hist daily growth recovered prev week   = DIVIDE(SUM('data_ot'[recovered]), SUM('data_ot'[lag_7_recovered]))^(1/7) -1
-hist daily growth deaths prev week      = DIVIDE(SUM('data_ot'[deaths]   ), SUM('data_ot'[lag_7_deaths]   ))^(1/7) -1
-hist daily growth active prev week      = DIVIDE(SUM('data_ot'[active]   ), SUM('data_ot'[lag_7_active]   ))^(1/7) -1
+
+positive test rate total           = DIVIDE(SUM('data_at'[confirmed])                                , SUM('data_at'[tested])                               )                                
+positive test rate prev day        = DIVIDE(SUM('data_at'[confirmed])-SUM('data_at'[lag_1_confirmed]), SUM('data_at'[tested]) - SUM('data_at'[lag_1_tested]))
+positive test rate prev week       = DIVIDE(SUM('data_at'[confirmed])-SUM('data_at'[lag_7_confirmed]), SUM('data_at'[tested]) - SUM('data_at'[lag_7_tested]))
+hist positive test rate            = DIVIDE(SUM('data_ot'[confirmed])                                , SUM('data_ot'[tested])                               )                                
+hist positive test rate prev day   = DIVIDE(SUM('data_ot'[confirmed])-SUM('data_ot'[lag_1_confirmed]), SUM('data_ot'[tested]) - SUM('data_ot'[lag_1_tested]))
+hist positive test rate prev week  = DIVIDE(SUM('data_ot'[confirmed])-SUM('data_ot'[lag_7_confirmed]), SUM('data_ot'[tested]) - SUM('data_ot'[lag_7_tested]))
