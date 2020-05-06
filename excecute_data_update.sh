7e809903e9463f7b47278791a9ed933f930d6648
v4lue4dded <v4lue4dded@gmail.com>
2020-05-06 16:01:41 +0100

    sudo

--- excecute_data_update.sh
+++ excecute_data_update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git submodule foreach --recursive 'git pull'
-python merge_tables.py
-git add COVID-19
-git add covid-19-data
-git add df_data_clean.tsv
-git add df_data_clean_max_date.tsv
-git add df_lu_clean.tsv
-git commit -m "date update: `date +'%Y-%m-%d %H:%M:%S'`";
-git push origin master
+sudo git submodule foreach --recursive 'git pull'
+sudo python merge_tables.py
+sudo git add COVID-19
+sudo git add covid-19-data
+sudo git add df_data_clean.tsv
+sudo git add df_data_clean_max_date.tsv
+sudo git add df_lu_clean.tsv
+sudo git commit -m "date update: `date +'%Y-%m-%d %H:%M:%S'`";
+sudo git push origin master
